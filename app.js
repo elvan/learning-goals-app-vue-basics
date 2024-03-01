9e46185d17ad90d3b322e431f4af7427030dc12d
Elvan Hidayat <elvanhd@gmail.com>
2024-03-01 21:02:25 +0700

    Revamp Vue app structure and styling
    - Simplify app data structure to use a goals array
    - Update app mounting point to reflect new goals feature
    - Replace goal output method with a dynamic list in index.html
    - Adjust HTML structure for adding and listing goals
    - Update CSS to align with new goals section and elements
    - Enhance header and goals section styling for better visual appeal
    - Implement more responsive design for input and button elements

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
 const app = Vue.createApp({
   data() {
-    return {
-      courseGoalA: 'Finish the course and learn Vue!',
-      courseGoalB: 'Master Vue and build amazing apps!',
-      vueLink: 'https://vuejs.org/',
-    };
-  },
-  methods: {
-    outputGoal() {
-      const randomNumber = Math.random();
-      if (randomNumber < 0.5) {
-        return this.courseGoalA;
-      } else {
-        return this.courseGoalB;
-      }
-    },
+    return { goals: [] };
   },
 });
 
-app.mount('#user-goal');
+app.mount('#user-goals');
